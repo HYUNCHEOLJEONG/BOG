b0a5a261503862a5b6b5e721a246f87ac1a6a7e7
HYUNCHEOL JEONG <hyuncheol1005@gmail.com>
2025-06-18 19:44:22 +0900

    BOG

--- C/나머지(10430)  .c
+++ C/나머지(10430)  .c
@@ -1,25 +1,24 @@
 
-1
 #include<stdio.h>
-2
+
 int main() {
-3
+
     int A, B, C;
-4
+
     scanf("%d", &A);
-5
+
     scanf("%d", &B);
-6
+
     scanf("%d", &C);
-7
+
     printf("%d\n", (A + B) % C);
-8
+
     printf("%d\n", ((A % C) + (B % C)) % C);
-9
+
     printf("%d\n", (A*B) % C);
-10
+
     printf("%d\n", ((A % C) * (B % C)) % C);
-11
+
 }
-12
-​
+
+
